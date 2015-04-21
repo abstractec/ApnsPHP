ddc375985cd5bac5327f9b605f4736d1e1fae8f8
John Haselden <john.haselden@abstractec.co.uk>
2015-04-21 19:56:44 +0100

    add an APNS push message implementation

---
+++ ApnsPHP/APNSMessage.php
@@ -0,0 +1,200 @@
+<?php
+/**
+ * @file
+ * ApnsPHP_Message class definition.
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://code.google.com/p/apns-php/wiki/License
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to aldo.armiento@gmail.com so we can send you a copy immediately.
+ *
+ * @author (C) 2010 Aldo Armiento (aldo.armiento@gmail.com)
+ * @version $Id$
+ */
+
+/**
+ * @defgroup ApnsPHP_Message Message
+ * @ingroup ApplePushNotificationService
+ */
+
+/**
+ * The Push Notification Message.
+ *
+ * The class represents a message to be delivered to an end user device.
+ * Notification Service.
+ *
+ * @ingroup ApnsPHP_Message
+ * @see http://tinyurl.com/ApplePushNotificationPayload
+ */
+class ApnsPHP_Message
+{
+	const PAYLOAD_MAXIMUM_SIZE = 256; /**< @type integer The maximum size allowed for a notification payload. */
+	const APPLE_RESERVED_NAMESPACE = 'aps'; /**< @type string The Apple-reserved aps namespace. */
+
+	protected $_bAutoAdjustLongPayload = true; /**< @type boolean If the JSON payload is longer than maximum allowed size, shorts message text. */
+
+	protected $_aDeviceTokens = array(); /**< @type array Recipients device tokens. */
+
+	protected $_sPushMagic; /**< @type string Alert message to display to the user. */
+
+	protected $_nExpiryValue = 604800; /**< @type integer That message will expire in 604800 seconds (86400 * 7, 7 days) if not successful delivered. */
+
+	/**
+	 * Constructor.
+	 *
+	 * @param  $sDeviceToken @type string @optional Recipients device token.
+	 */
+	public function __construct($sDeviceToken = null)
+	{
+		if (isset($sDeviceToken)) {
+			$this->addRecipient($sDeviceToken);
+		}
+	}
+
+	/**
+	 * Add a recipient device token.
+	 *
+	 * @param  $sDeviceToken @type string Recipients device token.
+	 * @throws ApnsPHP_Message_Exception if the device token
+	 *         is not well formed.
+	 */
+	public function addRecipient($sDeviceToken)
+	{
+		if (!preg_match('~^[a-f0-9]{64}$~i', $sDeviceToken)) {
+			throw new ApnsPHP_Message_Exception(
+				"Invalid device token '{$sDeviceToken}'"
+			);
+		}
+		$this->_aDeviceTokens[] = $sDeviceToken;
+	}
+
+	/**
+	 * Get a recipient.
+	 *
+	 * @param  $nRecipient @type integer @optional Recipient number to return.
+	 * @throws ApnsPHP_Message_Exception if no recipient number
+	 *         exists.
+	 * @return @type string The recipient token at index $nRecipient.
+	 */
+	public function getRecipient($nRecipient = 0)
+	{
+		if (!isset($this->_aDeviceTokens[$nRecipient])) {
+			throw new ApnsPHP_Message_Exception(
+				"No recipient at index '{$nRecipient}'"
+			);
+		}
+		return $this->_aDeviceTokens[$nRecipient];
+	}
+
+	/**
+	 * Get the number of recipients.
+	 *
+	 * @return @type integer Recipient's number.
+	 */
+	public function getRecipientsNumber()
+	{
+		return count($this->_aDeviceTokens);
+	}
+
+	/**
+	 * Get all recipients.
+	 *
+	 * @return @type array Array of all recipients device token.
+	 */
+	public function getRecipients()
+	{
+		return $this->_aDeviceTokens;
+	}
+
+
+
+	/**
+	 * Set the auto-adjust long payload value.
+	 *
+	 * @param  $bAutoAdjust @type boolean If true a long payload is shorted cutting
+	 *         long text value.
+	 */
+	public function setAutoAdjustLongPayload($bAutoAdjust)
+	{
+		$this->_bAutoAdjustLongPayload = (boolean)$bAutoAdjust;
+	}
+
+	/**
+	 * Get the auto-adjust long payload value.
+	 *
+	 * @return @type boolean The auto-adjust long payload value.
+	 */
+	public function getAutoAdjustLongPayload()
+	{
+		return $this->_bAutoAdjustLongPayload;
+	}
+
+	/**
+	 * PHP Magic Method. When an object is "converted" to a string, JSON-encoded
+	 * payload is returned.
+	 *
+	 * @return @type string JSON-encoded payload.
+	 */
+	public function __toString()
+	{
+		try {
+			$sJSONPayload = $this->getPayload();
+		} catch (ApnsPHP_Message_Exception $e) {
+			$sJSONPayload = '';
+		}
+		return $sJSONPayload;
+	}
+
+	/**
+	 * Convert the message in a JSON-encoded payload.
+	 *
+	 * @throws ApnsPHP_Message_Exception if payload is longer than maximum allowed
+	 *         size and AutoAdjustLongPayload is disabled.
+	 * @return @type string JSON-encoded payload.
+	 */
+	public function getPayload()
+	{
+		return "{\"aps\": {}, \"mdm\" : \"".$this->_sPushMagic."\"}";
+	}
+
+	/**
+	 * Set the expiry value.
+	 *
+	 * @param  $nExpiryValue @type integer This message will expire in N seconds
+	 *         if not successful delivered.
+	 */
+	public function setExpiry($nExpiryValue)
+	{
+		if (!is_int($nExpiryValue)) {
+			throw new ApnsPHP_Message_Exception(
+				"Invalid seconds number '{$nExpiryValue}'"
+			);
+		}
+		$this->_nExpiryValue = $nExpiryValue;
+	}
+
+	/**
+	 * Get the expiry value.
+	 *
+	 * @return @type integer The expire message value (in seconds).
+	 */
+	public function getExpiry()
+	{
+		return $this->_nExpiryValue;
+	}
+
+	public function setPushMagic($mPushMagic)
+	{
+		$this->_sPushMagic = $mPushMagic;
+	}
+
+	public function getPushMagic()
+	{
+		return $this->_sPushMagic;
+	}
+}
